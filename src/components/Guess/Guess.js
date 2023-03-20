a2ecb477c6b9cb67c99f29cc3ccfbd61439cfb6a
Pham Huu Hien <phhien203@gmail.com>
2023-03-20 21:39:45 +0700

    feat: add game logic

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,13 +1,23 @@
 import React from "react";
+import { checkGuess } from "../../game-helpers";
 import { range } from "../../utils";
 
-function Guess({ value }) {
+function Cell({ letter, status }) {
+  const className = status ? `cell ${status}` : `cell`;
+  return <span className={className}>{letter}</span>;
+}
+
+function Guess({ value, answer }) {
+  const result = checkGuess(value, answer);
+
   return (
     <p className="guess">
       {range(5).map((num) => (
-        <span key={num} className="cell">
-          {value ? value[num] : ""}
-        </span>
+        <Cell
+          key={num}
+          letter={result ? result[num].letter : ""}
+          status={result ? result[num].status : ""}
+        />
       ))}
     </p>
   );
