e4dd84d2e443e506922606621798d4a35438e3be
Pham Huu Hien <phhien203@gmail.com>
2023-03-21 16:41:24 +0700

    feat: add banners

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,10 +1,12 @@
 import React from "react";
 
-import { sample } from "../../utils";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 import { WORDS } from "../../data";
+import { sample } from "../../utils";
 import GuessInput from "../GuessInput/GuessInput";
 import GuessResults from "../GuessResults/GuessResults";
-import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
+import LostBanner from "../LostBanner/LostBanner";
+import WonBanner from "../WonBanner/WonBanner";
 
 // Pick a random word on every page load.
 const answer = sample(WORDS);
@@ -32,6 +34,8 @@ function Game() {
     <>
       <GuessResults guesses={guesses} answer={answer} />
       <GuessInput gameStatus={gameStatus} addNewGuess={onAddNewGuess} />
+      {gameStatus === "won" && <WonBanner numOfGuesses={guesses.length} />}
+      {gameStatus === "lost" && <LostBanner answer={answer} />}
     </>
   );
 }
